dbd2e4988b2213268259e630f67a2f62a5acc4ea
Ruiting Chen <1220327752@qq.com>
2025-05-11 01:48:46 +0800

    Problem 2 Completed

--- scheme_eval_apply.py
+++ scheme_eval_apply.py
@@ -44,11 +44,21 @@ def scheme_apply(procedure, args, env):
        assert False, "Not a Frame: {}".format(env)
     if isinstance(procedure, BuiltinProcedure):
         # BEGIN PROBLEM 2
-        "*** YOUR CODE HERE ***"
+        args_list = []
+        reminder = args
+        while reminder is not nil:
+            if reminder.first is not nil:
+                args_list.append(reminder.first)
+            else:
+                args_list.append(nil)
+            reminder = reminder.rest
+
+        if procedure.need_env:
+            args_list.append(env)
         # END PROBLEM 2
         try:
             # BEGIN PROBLEM 2
-            "*** YOUR CODE HERE ***"
+            return procedure.py_func(args_list)
             # END PROBLEM 2
         except TypeError as err:
             raise SchemeError('incorrect number of arguments: {0}'.format(procedure))
